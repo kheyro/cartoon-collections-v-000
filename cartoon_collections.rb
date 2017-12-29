849212a1dee70cf1d84588d72ccbde9a348b1986
Denis Atkesone <denis.atkesone@gmail.com>
2017-12-29 23:44:04 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def long_planeteer_calls(collection)
   collection.any? { |collection| collection.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(food)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  food.include?(cheese_types)
 end
